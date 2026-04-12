600f3f633c8a950214d396de99df6deec2edf4c1
shantalshanty <charletshantal@yahoo.com>
2026-04-12 05:48:47 +0100

    Fix Render host binding

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/', (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
